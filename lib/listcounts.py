e4c7639da6650a081c881e200119f4d4e689e13b
itgsod-filip-forsberg <filip.forsberg@elev.it-gymnasiet.se>
2015-11-03 11:04:53 +0100

    har gjort allting fram tills medianen

--- lib/listcounts.py
+++ lib/listcounts.py
@@ -1,1 +1,31 @@
-hejsan
+def sum(lista):
+    summa = 0
+    for i in lista:
+        summa += i
+    return summa
+
+def min(lista):
+    mini = None
+    for i in lista:
+        if mini is None or mini > i:
+             mini = i
+
+
+def max(lista):
+    maxi = None
+    for i in lista:
+        if maxi is None or maxi < i:
+            maxi = i
+
+def average(lista):
+    tal = 0
+    summa = 0
+    medelvarde = 0
+    for i in lista:
+        tal += 1
+        summa += i
+    medelvarde = summa/tal
+    return medelvarde
+
+def median(lista):
+    
