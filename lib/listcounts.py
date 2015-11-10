f20e85c45d0850d2580ed643f4484a0ae3c32a73
itgsod-filip-forsberg <filip.forsberg@elev.it-gymnasiet.se>
2015-11-10 09:53:20 +0100

    Klar med uppgiften

--- lib/listcounts.py
+++ lib/listcounts.py
@@ -1,26 +1,35 @@
 def sum(lista):
+    if len(lista) == 0:
+        raise ValueError
     summa = 0
     for i in lista:
         summa += i
     return summa
 
 def min(lista):
+    if len(lista) == 0:
+        raise ValueError
     mini = None
     for i in lista:
         if mini is None or mini > i:
              mini = i
+    return mini
 
 
 def max(lista):
+    if len(lista) == 0:
+        raise ValueError
     maxi = None
     for i in lista:
         if maxi is None or maxi < i:
             maxi = i
+    return maxi
 
 def average(lista):
+    if len(lista) == 0:
+        raise ValueError
     tal = 0
-    summa = 0
-    medelvarde = 0
+    summa = 0.0
     for i in lista:
         tal += 1
         summa += i
@@ -28,4 +37,20 @@ def average(lista):
     return medelvarde
 
 def median(lista):
-    
+    if len(lista) == 0:
+        raise ValueError
+    antal_saker = 0
+    for i in lista:
+        antal_saker += 1
+    median = antal_saker/2
+    if antal_saker % 2 == 1:
+        return sorted(lista)[median]
+    elif len(lista) == 1:
+        return lista[0]
+    else:
+        value_nonfloat_h = median
+        value_nonfloat_v = median - 1
+        median_nonfloatlist = (sorted(lista)[value_nonfloat_v] + sorted(lista)[value_nonfloat_h])/2.0
+        return median_nonfloatlist
+
+
